71d07fb81556dc44b8cf849c88a49bf1f849edbc
yangzhang31 <yangz15@nycstudents.net>
2024-11-16 19:46:44 -0500

    as

--- Zhang_Yang_Homework_4.cpp
+++ Zhang_Yang_Homework_4.cpp
@@ -10,10 +10,12 @@
 using namespace std;
 
 void filterEvens(int myArray[], int arraySize); //Prototype void function called filterEvens taking an array value and an array size value
+void dna_to_rna (string dna); 
 
 int main() {
 //Exercise One
     cout << endl; //Seperates the terminal into readable parts
+    cout << " ------------------------- Exercise One ------------------------- " << endl;
     int myArray[8]; //Declares the array with 8 boxes
 
     for (int i = 0; i < 8; i++) { //For loot using i 
@@ -21,6 +23,27 @@ int main() {
         cin >> myArray[i]; //Inputs the value into the corresponding array box
     }
     filterEvens(myArray, 8); //Calls on the function 
+
+//Exercise Two
+    cout << endl; //Seperates the terminal into readable parts
+    cout << " ------------------------- Exercise Two ------------------------- " << endl;
+    int cars[10] = {7, 3, 6, 0, 14, 8, 1, 2, 9, 8};
+    int totalCars = 0;
+    int maxCars = 0;
+    int bestSalesMan;
+
+    for (int i = 0; i < 10; i++) {
+        totalCars = totalCars + cars[i];
+        if (cars[i] > maxCars) {
+            maxCars = cars[i];
+            bestSalesMan = i + 1;
+        }
+    }
+    cout << "Total of " << totalCars << " cars were sold. The best sales person was sales person #" << bestSalesMan << " with " << maxCars << " cars sold.";
+
+//Exercise Four
+    cout << endl; //Seperates the terminal into readable parts
+    cout << " ------------------------- Exercise Four ------------------------- " << endl;
 
     return 0;
 }
